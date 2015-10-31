b3c307d40a4f41603eb406c0fee2ca5a8a48596e
Hannes Kirsman <hannes.kirsman@wunderkraut.com>
2015-10-31 23:38:07 +0200

    Shorter js to show the high contrast link.

--- js/high_contrast_link.js
+++ js/high_contrast_link.js
@@ -1,6 +1,4 @@
 (function($) {
-  $skipLink = $('#skip-link');
-  if ($skipLink.length) {
-    $skipLink.append(Drupal.settings.highContrast.link);
-  }
+  // Create high contrast link.
+  $('#skip-link').append(Drupal.settings.highContrast.link);
 })(jQuery);
